74755127592539f05f898f9e9baf2440632bf65c
Joel DeSante <joeldesante@gmail.com>
2026-01-31 16:03:41 -0500

    Add account creation logic

--- src/stories/pages/auth/signup/steps/FormStep.svelte
+++ src/stories/pages/auth/signup/steps/FormStep.svelte
@@ -1,19 +1,48 @@
-<script>
+<script lang="ts">
+	import { authClient } from "$lib/client";
+	import { nanoid } from "nanoid";
 	import Button from "../../../../components/inputs/button/Button.svelte";
 	import Input from "../../../../components/inputs/input/Input.svelte";
 
+    interface Props {
+        onComplete: (username: string, password: string) => void
+    }
+
+    let { onComplete = (username: string, password: string) => {} } = $props();
+
     let username = $state("");
     let password = $state("");
-    let confirm_password = $state("");
+    let confirmPassword = $state("");
+    let errorMessage = $state("");
 
-    function onSubmit() {
-        console.log(username, password)
+    async function onSubmit() {
+        try {
+            if(password != confirmPassword) {
+                throw new Error("Passwords do not match.")
+            }
+
+            const { error } = await authClient.signUp.email({
+                email: `${nanoid()}@fake.com`,
+                name: username,
+                username: username,
+                password: password,
+            });
+
+            if(error) {
+                throw new Error(error.message)
+            }
+
+            onComplete(username, password);
+        } catch(e: any) {
+            errorMessage = e.message;
+        }
     }
 
 </script>
 <div class="flex flex-col gap-4">
     <Input type="text" label="Username" bind:value={username} />
     <Input type="password" label="Password" bind:value={password} />
-    <Input type="password" label="Confirm Password" bind:value={confirm_password} />
+    <Input type="password" label="Confirm Password" bind:value={confirmPassword} />
     <Button label="Sign In" onclick={onSubmit} />
+    <p>{errorMessage}</p>
 </div>
