dfd4a912a68430c81490cfc4735b99079499f158
Chloe Atchue-Mamlet <chloe793@gmail.com>
2026-02-05 11:39:59 -0500

    Change colors from raw hex values to theme variables

--- src/routes/system/turfs/+page.svelte
+++ src/routes/system/turfs/+page.svelte
@@ -7,13 +7,13 @@
 </script>
 
 <div>
-    <div class="grid grid-cols-3 gap-2 border-b-1">
+    <div class="grid grid-cols-3 gap-2 border-b">
       <p>Code</p>
       <p>Created</p>
       <p>Expires</p>
     </div>
   {#each turfs as turf}
-    <div class="grid grid-cols-3 gap-2 border-b-1">
+    <div class="grid grid-cols-3 gap-2 border-b">
       <p>{turf.code}</p>
       <p>{turf.created_at}</p>
       <p>{turf.expires_at}</p>
